04b0d8060f279903485d173c9675a41c2edc04e3
Klebersonkvc <skat8kvc@hotmail.com>
2026-09-19 18:04:31 -0300

    feat: adiciona instrumentacao Prometheus (prom-client, rota /metrics, monitoring/prometheus.yml e dashboard Grafana)

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,8 +3,19 @@ const categoryRoutes = require('./category.routes');
 const productRoutes = require('./product.routes');
 const customerRoutes = require('./customer.routes');
 const orderRoutes = require('./order.routes');
+const metricsMiddleware = require('../middlewares/metrics');
+const { register } = require('../config/metrics');
 
 const router = Router();
+
+// Instrumenta todas as requisições que passam por este router
+router.use(metricsMiddleware);
+
+// Rota exposta para o Prometheus fazer o scrape das métricas
+router.get('/metrics', async (req, res) => {
+  res.setHeader('Content-Type', register.contentType);
+  res.send(await register.metrics());
+});
 
 router.use('/categories', categoryRoutes);
 router.use('/products', productRoutes);
